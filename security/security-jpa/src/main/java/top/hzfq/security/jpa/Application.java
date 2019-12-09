43a98724b75bab2118eed6fceed4128ae5da4a0c
hzfq <huzhifengqing@qq.com>
2019-12-09 15:55:54 +0800

    add security project

---
+++ security/security-jpa/src/main/java/top/hzfq/security/jpa/Application.java
@@ -0,0 +1,18 @@
+package top.hzfq.security.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hzfq
+ * @email huzhifengqing@qq.com
+ * @date 2019/11/28
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
